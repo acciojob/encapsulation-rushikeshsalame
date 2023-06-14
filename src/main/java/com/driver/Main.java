831e69a5948613525f903704fa4cd637c5a980da
Rushikesh Salame <rushikeshsalame@gmail.com>
2023-06-14 17:43:04 +0530

    Added solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
-  
+
+    RWOnly obj = new RWOnly();
+
+    //obj.name = "Rushikesh";
+    obj.setName("Rushikesh");
+    obj.getName();
+
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class RWOnly {
-    
+
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+    public void setName(){
+        this.name=name;
+    }
+
 }
